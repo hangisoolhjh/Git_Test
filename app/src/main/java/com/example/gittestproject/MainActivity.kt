346ff8f96a5d5addc8eb5aa8c9ed24c1c1cfffba
hangisoolCYS <cys020628@hangisool.co.kr>
2022-12-21 10:47:54 +0900

    qweads 함수 추가22

--- app/src/main/java/com/example/gittestproject/MainActivity.kt
+++ app/src/main/java/com/example/gittestproject/MainActivity.kt
@@ -2,15 +2,17 @@ package com.example.gittestproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        //push 1차 test 진행
-        //push 2차 test 진행
-        asdasdasdasd
-        gdfgdfhdfhasdasdasdasdsad
+        qweads()
+    }
+
+    private fun qweads() {
+        Log.e("qweasd", "QWeas")
     }
 }
