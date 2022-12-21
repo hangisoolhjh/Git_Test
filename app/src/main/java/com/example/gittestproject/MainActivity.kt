444828d764e6d42117add32b332b74eabc16e790
hangisoolCYS <cys020628@hangisool.co.kr>
2022-12-21 10:51:05 +0900

    qweads 함수 추가22`11

--- app/src/main/java/com/example/gittestproject/MainActivity.kt
+++ app/src/main/java/com/example/gittestproject/MainActivity.kt
@@ -9,7 +9,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        qweads()
+        qweads()213
     }
 
     private fun qweads() {
