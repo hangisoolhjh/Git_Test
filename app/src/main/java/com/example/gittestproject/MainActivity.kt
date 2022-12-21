e505f82840fce9084c0084e59d7d697a03c945e8
hangisoolhjh <arbert14@hangisool.co.kr>
2022-12-21 10:11:13 +0900

    push test

--- app/src/main/java/com/example/gittestproject/MainActivity.kt
+++ app/src/main/java/com/example/gittestproject/MainActivity.kt
@@ -7,5 +7,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //push 1차 test 진행
     }
 }
